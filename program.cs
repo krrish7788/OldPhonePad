79f687728956b7be1e82b656e27bb1f606d4a698
krrish7788 <58386008+krrish7788@users.noreply.github.com>
2023-03-21 00:42:43 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -6,11 +6,11 @@ using System.Collections.Generic;
 namespace MyApplication
 {
   class Program {
-  	private static readonly Dictionary<char, string> KEYS = new Dictionary<char, string>() {
-    	{'1', "&'("}, {'2', "ABC"}, {'3', "DEF"}, {'4', "GHI"}, {'5', "JKL"}, 
-        {'6', "MNO"}, {'7', "PQRS"}, {'8', "TUV"}, {'9', "WXYZ"}, {'0', " "}
-	};
-	public static string OldPhonePad(string input) {
+    private static readonly Dictionary<char, string> KEYS = new Dictionary<char, string>() {
+       {'1', "&'("}, {'2', "ABC"}, {'3', "DEF"}, {'4', "GHI"}, {'5', "JKL"}, 
+       {'6', "MNO"}, {'7', "PQRS"}, {'8', "TUV"}, {'9', "WXYZ"}, {'0', " "}
+    };
+    public static string OldPhonePad(string input) {
       StringBuilder sb = new StringBuilder();
       char prevChar = ' ';
       int consecutiveCount = 0;
